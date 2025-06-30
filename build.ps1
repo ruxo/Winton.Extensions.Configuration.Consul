44a2594e5ecf827a2cfa9c49b8873015b5035064
Ruxo Zheng <639905+ruxo@users.noreply.github.com>
2025-06-30 17:12:04 +0700

    Versioning

---
+++ build.ps1
@@ -0,0 +1,19 @@
+$destination = $args[0]
+
+function build {
+    param ($path)
+    Write-Output "Target: $path"
+    Push-Location $path
+    Remove-Item ./bin/Release/*.nupkg
+    dotnet pack -c Release
+    $file = Get-Item ./bin/Release/*.nupkg
+    Write-Output "Build $($file.Name)"
+    Move-Item -Force ./bin/Release/*.nupkg $destination
+    Pop-Location
+}
+
+Push-Location ./src
+
+build .\Winton.Extensions.Configuration.Consul
+
+Pop-Location
